cf15793c64fedf5eb17552cdaf9bdb41478de908
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-07-12 23:16:24 -0600

    i think i solved the second question

--- higher-order-function-challenge.js
+++ higher-order-function-challenge.js
@@ -4,7 +4,9 @@
 
 //loop through array and find if at least one value is greater then 10
 
-function any(num, ifLarger10)
+console.log("Question 1");
+
+function any(num, callback)
 {
   for (var i = 0; i < num.length; i++)
   {
@@ -22,3 +24,25 @@ var greaterThan10 = function()
 }
 
 console.log(any([ 8, 9, 10, 11 ], greaterThan10));
+
+// surprise()()
+// 'surprise!'
+// Return a function
+
+
+
+function surprise()
+{
+  return function secondFunction()
+  {
+    return "surprise!";
+  }
+}
+
+console.log(surprise()());
+
+// filter([ 1, 2, 3, 4 ], onlyOdd)
+// [ 1, 3 ]
+// onlyOdd is a callback
+
+console.log("Question 2");
