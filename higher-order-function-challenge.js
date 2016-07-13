e6f8a75f2491de20c6512c9ccedaf22c49b15d11
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-07-12 23:37:57 -0600

    completed homework, my programs work, but i am for sure using functions incorrectly and dont understand

--- higher-order-function-challenge.js
+++ higher-order-function-challenge.js
@@ -29,7 +29,7 @@ console.log(any([ 8, 9, 10, 11 ], greaterThan10));
 // 'surprise!'
 // Return a function
 
-
+console.log("Question 2");
 
 function surprise()
 {
@@ -45,4 +45,46 @@ console.log(surprise()());
 // [ 1, 3 ]
 // onlyOdd is a callback
 
-console.log("Question 2");
+console.log("Question 3");
+
+function filter(findOdd, callback2)
+{
+  var odds = [];
+  for (var i = 0; i < findOdd.length; i++)
+  {
+    if (findOdd[i] % 2 !== 0)
+    {
+      var odd = findOdd[i];
+      odds.push(odd);
+
+      // return onlyOdd;
+      // wtf is onlyOdd for, and how do I use it
+    }
+  }
+  return odds;
+}
+
+// var onlyOdd = function()
+// {
+//   return odds;
+// }
+
+console.log(filter([ 1, 2, 3, 4 ]));
+// supposed to be
+// console.log(filter([ 1, 2, 3, 4 ], onlyOdd));
+
+// sumTwoNumbers(3)(9)
+// 12
+// Return a function
+
+console.log("Question 4");
+
+function sumTwoNumbers(first)
+{
+  return function sumTwoNumbersSecond(second)
+  {
+    return first + second;
+  }
+}
+
+console.log(sumTwoNumbers(3)(9));
